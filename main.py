2198b12265f6975eedda9ad1424ed5f6366cc358
osbm <osmanfbayram@gmail.com>
2023-09-28 04:18:35 +0300

    fix

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 from learn_images.trainer import train
-from learn_images.models import SimpleMLP, SkipConnectionsMLP, Fourier2DFeatures, FourierFeatues, PadeFeatures
-
+from learn_images.models import FourierFeatures, SimpleMLP, GaussianFourierMapping
+from learn_images.utils import generate_lin_space
 from torch import nn
 import torch
-# model = SimpleMLP(hidden_size=100, num_hidden_layers=7, init_size=2, output_size=3)
+# import matplotlib.image
+# from PIL import Image
+# import numpy as np
 
-# feature_extractor = FourierFeatues(fourier_order=4)
-feature_extractor = Fourier2DFeatures(fourier_order=4)
-model = SkipConnectionsMLP(
-    hidden_size=100,
-    num_hidden_layers=7,
-#     init_size=2,
-    init_size=feature_extractor.output_shape,
-    output_size=3
-)
-model = nn.Sequential(feature_extractor, model)
+feature_extractor = GaussianFourierMapping()
+model = SimpleMLP(hidden_size=256, num_hidden_layers=4, init_size=512, output_size=3, output_activation="sigmoid")
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
-# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
 
 train(
-    experiment_name="fourier2dskipconn",
+    experiment_name="gaussian_fourier_mapping",
     image_path="data/target.jpeg",
     output_folder="output_folder",
     model=model,
+    feature_extractor=feature_extractor,
     optimizer=optimizer,
     scheduler=scheduler,
-    max_epochs=3000,
+    max_epochs=1000,
     early_stopping_patience=50,
-    save_every=5,
+    save_every=1,
     disable_wandb=False,
 )
