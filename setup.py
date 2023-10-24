e243931b1c387bfb8f6bf4984b6458ffc6b53ac6
osbm <osmanfbayram@gmail.com>
2023-10-24 18:24:32 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='learn_images',
-    version='0.0.1',
+    version='0.0.2',
     description='Module containing my experiments',
     long_description=open('README.md').read(),
     packages=find_packages(exclude=['scripts']),
