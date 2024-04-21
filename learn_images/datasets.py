aedc8aa567b99bfa2f91900501738781d03d3b5b
osbm <osmanfbayram@gmail.com>
2024-04-22 02:59:19 +0300

    add video dataset

--- learn_images/datasets.py
+++ learn_images/datasets.py
@@ -41,7 +41,6 @@ class VideoDataset(torch.utils.data.Dataset):
         images = os.listdir(images_folder)
         images = sorted(images, key=lambda x: int(x.split('_')[1].split('.')[0]))
         self.images = [os.path.join(images_folder, image) for image in images]
-        self.len = len(self.images)
 
     def __len__(self):
         return len(self.images)
@@ -54,8 +53,55 @@ class VideoDataset(torch.utils.data.Dataset):
         image = image.to(torch.float32)
         image = image / 255.0
 
-        input_tensor = idx / self.len
+        input_tensor = idx / len(self)
         input_tensor = torch.tensor(input_tensor, dtype=torch.float32)
         input_tensor = input_tensor.unsqueeze(-1)
         return input_tensor, image
 
+def generate_3d_linear_space(image_size=(28, 28), num_frames=10):
+    # generate 3d linear space of coordinates
+    # numbers will be between 0 and 1
+
+    X = torch.linspace(0, 1, image_size[0])
+    Y = torch.linspace(0, 1, image_size[1])
+    Z = torch.linspace(0, 1, num_frames)
+    grid = torch.stack(torch.meshgrid(X, Y, Z, indexing='ij'))
+    grid = grid.permute(1, 2, 3, 0)
+    # flatten the first three dimensions
+    grid = grid.flatten(0, 2)
+        
+    return grid
+
+class VideoPixelDataset(torch.utils.data.Dataset):
+    def __init__(self, images_folder, convert_to="L"):
+        print("Loading images...")
+        images = sorted(os.listdir(images_folder), key=lambda x: int(x.split('_')[1].split('.')[0]))
+        self.images = [os.path.join(images_folder, image) for image in images]
+        self.images = [load_image(image, convert_to=convert_to) for image in self.images]
+        self.images = torch.stack(self.images)
+        self.images = self.images.to(torch.float32)
+        self.images = self.images / 255.0
+        self.num_images, self.image_x, self.image_y = self.images.shape
+        self.images = self.images.flatten(0, 2)
+        print("Done loading images...")
+    
+
+        print("Generating linear space...")
+        self.linear_space = generate_3d_linear_space(
+            image_size=(self.image_x, self.image_y),
+            num_frames=self.num_images,
+        )
+        print("Done generating linear space...")
+
+    def __len__(self):
+        return self.num_images * self.image_x * self.image_y
+
+    def __getitem__(self, idx):
+        # it is a video dataset but we get a pixel at a time
+        # so our model will input (x, y, frame_id) and output pixel value
+        # everything is normalized between 0 and 1
+        # so there will be num_frames * image_x * image_y samples
+
+        input_tensor = self.linear_space[idx]
+        pixel_value = self.images[image_idx, pixel_x, pixel_y]
+        return input_tensor, pixel_value
